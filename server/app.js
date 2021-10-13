88ad16466bc509601fdc2c9f7f0ec0395c87f3e7
mgitto1 <matthew.gitto1@gmail.com>
2021-10-12 20:16:54 -0400

    added cors middleware

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 module.exports = app;
 
+app.use(cors);
 // logging middleware
 app.use(morgan('dev'));
 
